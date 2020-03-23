03a103fbf1993ab733a743a6a5552e01e83ac935
xiao <317722960@qq.com>
2020-03-23 18:08:46 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# pages
+## 个人博客
+采用的是国产的开源项目		
+[vuepress-theme-reco](https://github.com/vuepress-reco/vuepress-theme-reco)
+
+该项目是基于开源项目[vuepress](https://github.com/vuejs/vuepress)进行改造升级的.
+
+##文档说明
+该开源项目提供了详细的文档说明
+[vuepress-theme-reco](https://vuepress-theme-reco.recoluan.com/views/1.x/)
+
+##部署到Pages.GitHub.io
+参考文献：[使用GitHub Pages 建立博客](https://blog.csdn.net/qq_39268193/article/details/86028884)
+
---
+++ example/docs/.vuepress/config.js
@@ -0,0 +1,75 @@
+module.exports = {
+  title: "肖布斯博客",
+  description: '枪如惊雷 照一身肝胆',
+  dest: 'example/docs/public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  // theme: 'reco',
+  theme: require.resolve('../../../'),
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/', icon: 'reco-home' },
+      { text: '时间轴', link: '/timeline/', icon: 'reco-date' }
+      /* ,{ text: '联系我',
+        icon: 'reco-message',
+        items: [
+          { text: 'NPM', link: 'https://www.npmjs.com/~reco_luan', icon: 'reco-npm' },
+          { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' },
+          { text: '简书', link: 'https://www.jianshu.com/u/cd674a19515e', icon: 'reco-jianshu' },
+          { text: 'CSDN', link: 'https://blog.csdn.net/recoluan', icon: 'reco-csdn' },
+          { text: '博客圆', link: 'https://www.cnblogs.com/luanhewei/', icon: 'reco-bokeyuan' },
+          { text: 'WeChat', link: 'https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A', icon: 'reco-wechat' },
+        ]
+      } */
+    ],
+    type: 'blog',
+    // 博客设置
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: '分类' // 默认 “分类”
+      },
+      tag: {
+        location: 3, // 在导航栏菜单中所占的位置，默认3
+        text: '标签' // 默认 “标签”
+      }
+    },
+    logo: '/head.png',
+    authorAvatar: '/head.png',
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10,
+    // 自动形成侧边导航
+    sidebar: 'auto',
+    // 最后更新时间
+    lastUpdated: 'Last Updated',
+    // 作者
+    author: 'X-Man',
+    // 备案号
+    record: 'xxxx',
+    // 项目开始时间
+    startYear: '2020',
+    /**
+     * 密钥 (if your blog is private)
+     */
+    friendLink: [
+      {
+        title: '午后南杂',
+        desc: 'Enjoy when you can, and endure when you must.',
+        email: '1156743527@qq.com',
+        link: 'https://www.recoluan.com'
+      },
+      {
+        title: 'vuepress-theme-reco',
+        desc: 'A simple and beautiful vuepress Blog & Doc theme.',
+        avatar: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        link: 'https://vuepress-theme-reco.recoluan.com'
+      },
+    ]
+  },
+  markdown: {
+    // lineNumbers: true
+  }
+}
