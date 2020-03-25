44b3975bb2aa7437f475934427de5007d53ecfd8
xiao <317722960@qq.com>
2020-03-25 10:24:07 +0800

    更换目标服务器

--- example/docs/.vuepress/config.js
+++ example/docs/.vuepress/config.js
@@ -57,6 +57,11 @@ module.exports = {
      */
     friendLink: [
       {
+        title: '罗彪个人博客',
+        desc: '心怀不惧 方能翱翔于天际',
+        avatar: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        link: 'https://www.btluo.com'
+      },{
         title: '午后南杂',
         desc: 'Enjoy when you can, and endure when you must.',
         email: '1156743527@qq.com',
@@ -67,7 +72,7 @@ module.exports = {
         desc: 'A simple and beautiful vuepress Blog & Doc theme.',
         avatar: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
         link: 'https://vuepress-theme-reco.recoluan.com'
-      },
+      }
     ]
   },
   markdown: {
